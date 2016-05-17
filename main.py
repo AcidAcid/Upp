e3c50c74bcf9789823039e71ffd8cecc278e748a
ChoosingUsernameIsSoHard <badarchysayan@gmail.com>
2016-05-17 16:52:09 +0700

    Add template: empty for a while Rendering template

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Starting project"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
