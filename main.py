3cbb49e1e85865d977823a7b036022ca32b379bd
ChoosingUsernameIsSoHard <badarchysayan@gmail.com>
2016-05-17 16:39:28 +0700

    Add bootstrap files Add js files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +10,5 @@ def index():
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key = b'\x17\xe0U*\x04\xba\xd5~\x9d\x8aHcC\xf7\x84\x96\x7fWQ\xc4\x0c\xd9\x0f\xe9'
     app.run(port=8080)
