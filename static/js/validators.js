9d60da50187408d9bd25ca8ec515a1e36c94c97b
ChoosingUsernameIsSoHard <badarchysayan@gmail.com>
2016-05-22 22:03:03 +0700

    add validators on numbers

--- static/js/validators.js
+++ static/js/validators.js
@@ -1,5 +1,38 @@
+var input = $('#result');
+var label = $('label');
+
 $('#clearBtn').on('click',function(event) {
     // очищает текст метки
     // и переменную содержащую введенные данные
-    $('label').text("Empty");
+    label.text("Empty");
+    input.attr('value', "0");
+    entered = false;
+});
+
+$(".number").on('click', function(event) {
+    var btnText = $(this).text(); // текст нажатой кнопки
+    var value = input.attr('value');
+
+    if (value == '0'){
+        if (btnText == '0' || btnText == '00' ) {
+            value = '0';
+        } else if (btnText == '.') {
+            value += btnText;
+        } else {
+            value = btnText;
+        }
+    } else if (btnText == '.' && value.indexOf('.') != -1) {
+         return;
+    } else {
+        value += btnText;
+    }
+
+
+    input.attr('value', value);
+});
+
+$(".operators").on('click', function(event){
+    // проверить, ввёл ли пользователь число
+    // в значение метки записать число
+    // очищать input
 });
