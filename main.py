2c5a3247f8ad26ff69e4fa127abfc5d2ed6cd820
ChoosingUsernameIsSoHard <badarchysayan@gmail.com>
2016-05-24 11:09:23 +0700

    style: make main.py more readable

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.debug = True
     app.secret_key = b'\x17\xe0U*\x04\xba\xd5~\x9d\x8aHcC\xf7\x84\x96\x7fWQ\xc4\x0c\xd9\x0f\xe9'
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
